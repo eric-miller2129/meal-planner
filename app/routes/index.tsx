3077ba438d2af20d23fba46a2b500e73af1038b8
Eric Miller <emiller@tax-guard.com>
2022-01-25 13:09:14 -0700

    Added environment variables to the api for netlify

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,27 @@
+import axios from 'axios';
+import { ActionFunction, Form } from 'remix';
+
+export const action: ActionFunction = async ({ request }) => {
+  const body = await request.formData();
+  console.log(process.env.API_URL);
+  const { data } = await axios.post(`${ process.env.API_URL }/`, { url: body.get('url') });
+
+  return data;
+};
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <h1>Nutrition Thing</h1>
+      <Form method="post">
+        <div>
+          <label htmlFor="website">Recipe Url:</label>
+          <input type="text" id="website" name="url" />
+        </div>
+        <div>
+          <button type="submit">Get Nutrition</button>
+        </div>
+      </Form>
+    </>
   );
 }
